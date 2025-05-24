fddeea4bff7b138ee7d4390f15495e87ce813fc2
PedroSilva339 <pedro.silva339@fatec.sp.gov.br>
2025-05-24 19:33:00 -0300

    app_pastel_v1

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import Section_Venda from "./components/Section_venda";
+
+export default function app_Pastel() {
+  return(
+    <>
+      <Header/>
+      <Section_Venda/>
+    </>
+  )
+}
